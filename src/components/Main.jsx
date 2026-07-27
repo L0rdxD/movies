1cd12764452d6b028f05e6018e4bc365366d5fd1
L0rdxD <159791952+L0rdxD@users.noreply.github.com>
2026-07-27 17:19:09 +0300

    Добавление фильтрации

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,36 +1,40 @@
 import React from 'react';
 import { Movies } from './Movies';
 import { Search } from './Search';
-import { Preloader } from  './Preloader'
+import { Preloader } from './Preloader';
 
 class Main extends React.Component {
     state = {
         movies: [],
     };
 
-     searchMovies = (str) => {
-        fetch(`http://www.omdbapi.com/?apikey=1fde3c22&s=${str}`)
-            .then((response) => response.json())
-            .then((data) => this.setState({ movies: data.Search }));
-    };
-
     componentDidMount() {
         fetch('http://www.omdbapi.com/?apikey=1fde3c22&s=matrix')
             .then((response) => response.json())
-            .then((data) => this.setState({ movies: data.Search }));
+            // Добавлена проверка на случай ошибки API (data.Search может быть undefined)
+            .then((data) => this.setState({ movies: data.Search || [] }));
     }
+
+    searchMovies = (str, type = 'all') => { // <-- Исправлено: добавлена запятая после str
+        if (!str) return;
+        
+        fetch(
+            `http://www.omdbapi.com/?apikey=1fde3c22&s=${str}${
+                type !== 'all' ? `&type=${type}` : ''
+            }`
+        )
+            .then((response) => response.json())
+            // Добавлена проверка на случай ошибки API
+            .then((data) => this.setState({ movies: data.Search || [] }));
+    };
 
     render() {
         const { movies } = this.state;
-
+        
         return (
             <main className="container content">
                 <Search searchMovies={this.searchMovies} />
-                {movies.length ? (
-                    <Movies movies={this.state.movies} />
-                ) : (
-                    <Preloader />
-                )}
+                {!movies.length ? <Preloader /> : <Movies movies={movies} />}
             </main>
         );
     }
